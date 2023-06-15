ab986ebc04c0c44ddd7d92a689cdcf02d3827977
Enrico De Nicola <edenicola16@icloud.com>
2023-06-15 18:45:12 -0300

    Prueba

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Body from "../Components/Body.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Body />
+    </>
+  )
+}
+
+export default App
